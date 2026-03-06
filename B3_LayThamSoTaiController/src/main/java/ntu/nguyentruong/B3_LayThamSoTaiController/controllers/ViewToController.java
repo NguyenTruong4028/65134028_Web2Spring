b82b6f834f2143afb16464e2e0bd50e4878223a9
Nguyen Truong <truong.nh.4028@gmail.com>
2026-03-07 01:22:04 +0700

    done Bai 3

---
+++ B3_LayThamSoTaiController/src/main/java/ntu/nguyentruong/B3_LayThamSoTaiController/controllers/ViewToController.java
@@ -0,0 +1,20 @@
+package ntu.nguyentruong.B3_LayThamSoTaiController.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class ViewToController {
+	@GetMapping("/") //http://localhost:8080/?name=truong&que=camranh
+	public String getParametes(HttpServletRequest ts, ModelMap m) {
+		String name = ts.getParameter("name");
+		String quequan = ts.getParameter("que");
+		m.addAttribute("ten",name);
+		m.addAttribute("ns",quequan);
+		return "index";
+	}
+	
+}
