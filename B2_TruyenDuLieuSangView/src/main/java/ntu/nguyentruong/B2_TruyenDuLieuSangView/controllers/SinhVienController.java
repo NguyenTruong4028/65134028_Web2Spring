9750ec18d516fe9e8a0672913170b17b1beca390
Nguyen Truong <truong.nh.4028@gmail.com>
2026-03-07 00:50:11 +0700

    create Bai 2

---
+++ B2_TruyenDuLieuSangView/src/main/java/ntu/nguyentruong/B2_TruyenDuLieuSangView/controllers/SinhVienController.java
@@ -0,0 +1,5 @@
+package ntu.nguyentruong.B2_TruyenDuLieuSangView.controllers;
+
+public class SinhVienController {
+
+}
---
+++ B2_TruyenDuLieuSangView/src/test/java/ntu/nguyentruong/B2_TruyenDuLieuSangView/B2TruyenDuLieuSangViewApplicationTests.java
@@ -0,0 +1,13 @@
+package ntu.nguyentruong.B2_TruyenDuLieuSangView;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2TruyenDuLieuSangViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
