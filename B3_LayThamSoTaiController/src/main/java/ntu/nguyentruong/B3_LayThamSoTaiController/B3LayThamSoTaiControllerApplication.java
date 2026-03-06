26ec443215c24b5fe86a79b950237167ac414e79
Nguyen Truong <truong.nh.4028@gmail.com>
2026-03-07 01:11:31 +0700

    create Bai 3

---
+++ B3_LayThamSoTaiController/src/main/java/ntu/nguyentruong/B3_LayThamSoTaiController/B3LayThamSoTaiControllerApplication.java
@@ -0,0 +1,13 @@
+package ntu.nguyentruong.B3_LayThamSoTaiController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B3LayThamSoTaiControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B3LayThamSoTaiControllerApplication.class, args);
+	}
+
+}
---
+++ B3_LayThamSoTaiController/src/test/java/ntu/nguyentruong/B3_LayThamSoTaiController/B3LayThamSoTaiControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package ntu.nguyentruong.B3_LayThamSoTaiController;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B3LayThamSoTaiControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
