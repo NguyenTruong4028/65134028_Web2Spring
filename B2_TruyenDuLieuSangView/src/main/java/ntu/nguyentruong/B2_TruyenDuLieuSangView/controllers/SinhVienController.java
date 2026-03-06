bdd4863dced3adcc2a07e3396d3138c98105f28a
Nguyen Truong <truong.nh.4028@gmail.com>
2026-03-07 01:07:39 +0700

    done Bai 2

--- B2_TruyenDuLieuSangView/src/main/java/ntu/nguyentruong/B2_TruyenDuLieuSangView/controllers/SinhVienController.java
+++ B2_TruyenDuLieuSangView/src/main/java/ntu/nguyentruong/B2_TruyenDuLieuSangView/controllers/SinhVienController.java
@@ -1,5 +1,23 @@
 package ntu.nguyentruong.B2_TruyenDuLieuSangView.controllers;
 
-public class SinhVienController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class SinhVienController {
+	//TruyenDuLieuSangView
+	//truyền dữ liệu: MSSV, Họ tên SV, Năm sinh, Giới tính
+	@GetMapping("/")
+	public String SinhVien(ModelMap m) {
+		String name = "Nguyen Hong Truong";
+		String mssv = "65134028";
+		String ns = "2004";
+		String gtinh = "Nu";
+		m.addAttribute("name", name);
+		m.addAttribute("mssv", mssv);
+		m.addAttribute("ns", ns);
+		m.addAttribute("gt", gtinh);
+		return "sinhvien";
+	}
 }
