28053daf8e9a0e62309c11e8ce7b073f982fd02c
Nguyen Truong <truong.nh.4028@gmail.com>
2026-03-11 15:26:05 +0700

    create model Page + Post

---
+++ ntuCMS/src/main/java/ntu/nguyentruong/ntuCMS/models/Post.java
@@ -0,0 +1,55 @@
+package ntu.nguyentruong.ntuCMS.models;
+
+public class Post {
+	String id, title, content, categoryId, image;
+
+	public Post(String id, String title, String content, String categoryId, String image) {
+		this.id = id;
+		this.title = title;
+		this.content = content;
+		this.categoryId = categoryId;
+		this.image = image;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(String categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+	
+}
