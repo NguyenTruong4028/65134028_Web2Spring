f3b971031f397c623d9dadf820e7206a291e2c0c
Nguyen Truong <truong.nh.4028@gmail.com>
2026-03-11 14:37:24 +0700

    create ntuCMS

---
+++ ntuCMS/src/main/java/ntu/nguyentruong/ntuCMS/NtuCmsApplication.java
@@ -0,0 +1,13 @@
+package ntu.nguyentruong.ntuCMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtuCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NtuCmsApplication.class, args);
+	}
+
+}
---
+++ ntuCMS/src/test/java/ntu/nguyentruong/ntuCMS/NtuCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package ntu.nguyentruong.ntuCMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtuCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
