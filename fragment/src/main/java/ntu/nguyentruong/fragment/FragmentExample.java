a101bda663678ebea855f5e442219f89c0fb54c2
Nguyen Truong <truong.nh.4028@gmail.com>
2026-03-14 07:56:46 +0700

    done example

---
+++ fragment/src/main/java/ntu/nguyentruong/fragment/FragmentExample.java
@@ -0,0 +1,12 @@
+package ntu.nguyentruong.fragment;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FragmentExample {
+	@GetMapping("/")
+	public String getFragment() {
+		return "example";
+	}
+}
