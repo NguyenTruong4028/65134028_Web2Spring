7f0cfcc2aad215864f444d61eaa1e71ae592904d
Nguyen Truong <truong.nh.4028@gmail.com>
2026-03-21 09:34:54 +0700

    luyen tap bai thigk, co them service

---
+++ ntuCMS_Practice/src/main/java/ntu/nguyentruong/ntuCMS_Practice/NtuCmsPracticeApplication.java
@@ -0,0 +1,13 @@
+package ntu.nguyentruong.ntuCMS_Practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtuCmsPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NtuCmsPracticeApplication.class, args);
+	}
+
+}
---
+++ ntuCMS_Practice/src/test/java/ntu/nguyentruong/ntuCMS_Practice/NtuCmsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package ntu.nguyentruong.ntuCMS_Practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtuCmsPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
